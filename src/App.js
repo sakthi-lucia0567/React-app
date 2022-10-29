640fd833b36a9f8f9017538055954f592d01b518
sakthi-lucia0567 <sakthivel8725@outlook.com>
2022-10-29 22:50:02 +0530

    Styling The Web page using tailwind css

--- src/App.js
+++ src/App.js
@@ -6,23 +6,45 @@ import { useState } from "react";
 
 function App() {
   const hey = true;
-  const [role, setRole] = useState("dev");
+  const [role, setRole] = useState("developer");
+  //setRole("Web-developer");
 
   return (
     <div className="App text-black">
-      {console.log("Log")}
       {hey ? (
         <>
-          <input
-            type="text"
-            onChange={(e) => {
-              console.log(e.target.value);
-              setRole(e.target.value);
-            }}
-          />
-          <Employee name="Cellva" role="Stupid" />
-          <Employee name="Vikey" role={role} />
-          <Employee name="Sakti" />
+          <div className="flex flex-wrap justify-center">
+            <Employee
+              name="Cellva"
+              role={role}
+              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
+            />
+            <Employee
+              name="Vikey"
+              role={role}
+              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
+            />
+            <Employee
+              name="Sakthi"
+              role={role}
+              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
+            />
+            <Employee
+              name="Cellva"
+              role={role}
+              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
+            />
+            <Employee
+              name="Vikey"
+              role={role}
+              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
+            />
+            <Employee
+              name="Sakthi"
+              role={role}
+              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
+            />
+          </div>
         </>
       ) : (
         <p>Hello Sho</p>
--- src/Component/Employee.js
+++ src/Component/Employee.js
@@ -3,10 +3,22 @@ import React from "react";
 function Employee(props) {
   return (
     <>
-      <div>{props.name}</div>
-      {/* <p>{props.role ? props.role : "No Role"}</p> */}
-      {props.role ? <p>{props.role}</p> : <p>No Role</p>}
-      <div></div>
+      <div className="py-8 px-8 max-w-sm mt-2 ml-2 mr-2 mb-2 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img
+          className="block mx-auto  w-36 h-36 rounded-full rounded-full sm:mx-0 sm:shrink-0"
+          src={props.img}
+          alt="Woman's Face"
+        />
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">{props.name}</p>
+            <p className="text-slate-500 font-medium">{props.role}</p>
+          </div>
+          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Message
+          </button>
+        </div>
+      </div>
     </>
   );
 }
