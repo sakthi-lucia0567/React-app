c5d66af7f9317e6c05aa5ec543aa685f6f8138d4
sakthi-lucia0567 <sakthivel8725@outlook.com>
2022-10-29 23:28:36 +0530

    Map through State Array

--- src/App.js
+++ src/App.js
@@ -3,47 +3,60 @@ import "./index.css";
 import React from "react";
 import Employee from "./Component/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const hey = true;
   const [role, setRole] = useState("developer");
-  //setRole("Web-developer");
+  const [employees, setEmployees] = useState([
+    {
+      name: "cellva",
+      role: "senior dev",
+      img: "https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg",
+    },
+    {
+      name: "cellva",
+      role: "senior dev",
+      img: "https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg",
+    },
+    {
+      name: "cellva",
+      role: "senior dev",
+      img: "https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg",
+    },
+    {
+      name: "cellva",
+      role: "senior dev",
+      img: "https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg",
+    },
+    {
+      name: "cellva",
+      role: "senior dev",
+      img: "https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg",
+    },
+    {
+      name: "cellva",
+      role: "senior dev",
+      img: "https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg",
+    },
+  ]);
 
   return (
     <div className="App text-black">
       {hey ? (
         <>
           <div className="flex flex-wrap justify-center">
-            <Employee
-              name="Cellva"
-              role={role}
-              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
-            />
-            <Employee
-              name="Vikey"
-              role={role}
-              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
-            />
-            <Employee
-              name="Sakthi"
-              role={role}
-              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
-            />
-            <Employee
-              name="Cellva"
-              role={role}
-              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
-            />
-            <Employee
-              name="Vikey"
-              role={role}
-              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
-            />
-            <Employee
-              name="Sakthi"
-              role={role}
-              img="https://getwallpapers.com/wallpaper/full/d/c/c/1322240-naruto-uzumaki-wallpaper-2560x2048-notebook.jpg"
-            />
+            {employees.map((employee) => {
+              console.log(employees);
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
