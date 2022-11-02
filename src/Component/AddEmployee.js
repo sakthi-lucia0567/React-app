71a6b2fdc52ca381b9d47b49797a8aca20c93617
sakthi-lucia0567 <sakthivel8725@outlook.com>
2022-11-02 22:09:12 +0530

    Editing the employee

--- src/Component/AddEmployee.js
+++ src/Component/AddEmployee.js
@@ -4,6 +4,7 @@ import Modal from "react-bootstrap/Modal";
 function AddEmployee(props) {
   const [name, setName] = useState("");
   const [role, setRole] = useState("");
+  const [img, setImg] = useState("");
 
   const [show, setShow] = useState(false);
 
@@ -14,7 +15,7 @@ function AddEmployee(props) {
     <>
       <button
         onClick={handleShow}
-        className=" block mx-auto mt-2 bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className=" block mx-auto mt-2 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
       >
         + Add Employee
       </button>
@@ -46,8 +47,9 @@ function AddEmployee(props) {
               </div>
               <div className="md:w-2/3">
                 <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="name"
+                  placeholder="Sakthi"
                   type="text"
                   value={name}
                   onChange={(e) => {
@@ -64,10 +66,30 @@ function AddEmployee(props) {
               </div>
               <div className="md:w-2/3">
                 <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="role"
+                  placeholder="Gangsta"
                   type="text"
                   value={role}
+                  onChange={(e) => {
+                    setRole(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                  Image URL
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="role"
+                  placeholder="wwww.prettier.com"
+                  type="text"
+                  value={img}
                   onChange={(e) => {
                     setRole(e.target.value);
                   }}
