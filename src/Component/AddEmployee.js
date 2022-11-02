b0de4aeab35ae21d9a1fc87d9369fa2381ddb1d9
sakthi-lucia0567 <sakthivel8725@outlook.com>
2022-11-02 22:02:14 +0530

    Edit the add employee button

--- src/Component/AddEmployee.js
+++ src/Component/AddEmployee.js
@@ -14,7 +14,7 @@ function AddEmployee(props) {
     <>
       <button
         onClick={handleShow}
-        className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+        className=" block mx-auto mt-2 bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         + Add Employee
       </button>
@@ -89,9 +89,9 @@ function AddEmployee(props) {
           </button>
           <button
             form="editModel"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Update
+            Add
           </button>
         </Modal.Footer>
       </Modal>
