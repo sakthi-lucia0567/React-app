ded1a92e740a3e21ba675d72469bbf02efadd2d5
sakthi_l_u_c_i <sakthivel@gomobi.io>
2022-11-10 21:52:23 +0530

    revert changes

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -9,13 +9,6 @@ import { v4 as uuidv4 } from "uuid";
 import Header from "../Component/Header";
 
 function Employees() {
-  /**
-   * ! do we need to check
-   * ? hello world
-   * todo: yes this is intrusion
-   *
-   */
-
   const showEmployees = true;
   const [role, setRole] = useState("developer");
   const [employees, setEmployees] = useState([
