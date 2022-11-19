3de838eecd2d0827d5c28d09be796ae815afeb44
sakthi_l_u_c_i <sakthivel@gomobi.io>
2022-11-19 21:08:07 +0530

    hi

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -14,10 +14,6 @@ function Employees() {
   const [role, setRole] = useState("developer");
   const [employees, setEmployees] = useState([
     {
-      /**
-       * hello world
-      \
-       */
       id: 1,
       name: "cellva",
       role: "senior dev",
